5d07a0c56c6ed6e0c465998928ba73dd3b621f5c
Anastasia Ratcliff <aratclif@colostate.edu>
2018-11-01 10:30:18 -0600

    attemping one row per city

--- Writing/Script Homework Four.R
+++ Writing/Script Homework Four.R
@@ -1,3 +1,13 @@
+#load liabraries
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(lubridate)
+library(readr)
+library(stringr)
+
+
+
 # Read in the data as an R Object
 homicides <- homicide_data
 
@@ -9,7 +19,26 @@ homicides <- homicides %>%
 #one row per city that gives total homicides 
 #one row number of unsolved homicides 
 unsolved <- homicides %>%
-  select(city_name, disposition)
+  select(city_name, disposition)%>%
+  group_by(city_name)%>%
+  
+
+
+
+  
+  
+  
+  
+  mutate(unsolved_homicides = str_extract(disposition, "Closed without arrest|Open/No arrest",)) %>%
+  
+  group_by(city_name)
+  
+
+
+
+
+
+
 
 
 
