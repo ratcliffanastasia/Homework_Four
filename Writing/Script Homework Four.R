59e18fb476a022c81a8b271b0b46f6b9de9dbd4f
Anastasia Ratcliff <aratclif@colostate.edu>
2018-11-04 20:04:10 -0700

    change to factor

--- Writing/Script Homework Four.R
+++ Writing/Script Homework Four.R
@@ -15,30 +15,20 @@ homicides <- homicide_data
 
 #create new column called city_name
 homicides <- homicides %>%
-  unite(city_name, city, state, sep = ",", remove =FALSE )
-
+  unite(city_name, city, state, sep = ",", remove =FALSE )%>%
 
 
 #new dataframe
 #one row per city that gives total homicides 
+
 #one row number of unsolved homicides 
 unsolved <- homicides %>%
-  select(city_name, disposition) %>%
-  mutate(unsolved_homicides =  str_extract(disposition, "Closed without arrest|Open/No arrest"))%>%
-  group_by(city_name, disposition, unsolved_homicides)%>%
-  count(city_name)
- 
-    
-   
+  select(city_name, disposition)%>%
+  mutate(disposition = factor(disposition))%>%
+  mutate(disposition = fct_recode(disposition, '1' = 'Closed without arrest',
+                                  '1'= 'Open/No arrest',
+                                  '0' = 'Closed by arrest '))
   
-
-  
-  
-  
-  
-
-  
-  ?maps2
 
 
 
